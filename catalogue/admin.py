af69b8189a3bfec9476aa454011c202ec898dadd
JDRL555 <joshuleal3@gmail.com>
2024-04-07 17:30:45 -0400

    created the models and rendered the fruits

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from catalogue.models import Fruit, Category
+
 # Register your models here.
+admin.site.register(Fruit)
+admin.site.register(Category)
--- users/models.py
+++ users/models.py
@@ -1,20 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class register(models.user_register):
-    correo = models.EmailField()
-    password = models.PasswordField()
-    password_second = models.PasswordField()
-
-
-    def __str__(self):
-        return {self.correo}, {self.password}, {self.password_second}
-
-class login(models.user_login):
-    correo = models.EmailField()
-    password = models.PasswordField()
-
-    def __str__(self):
-        return {self.correo}, {self.password}
-    

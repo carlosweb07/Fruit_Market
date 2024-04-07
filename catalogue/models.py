adb1d3a3f96d534dc0f3cfbcec377486747aa0ba
gabrieljustdance <gabriel2043579@gmail.com>
2024-04-07 00:32:07 -0400

    models for user and catalogue

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+class acidas(models.acidas):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey()
+    avaible_units = models.FloatField(max_length=50)
+    price = models.FloatField(max_length=500)
+    img = models.ImageField(upload_to='DIRECION')
+
+    def __str__(self):
+        return f"{self.name} {self.category} {self.avaible_units} {self.price} {self.img}"
+    
+class semiacidas(models.semiacidas):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey()
+    avaible_units = models.FloatField(max_length=50)
+    price = models.FloatField(max_length=500)
+    img = models.ImageField(upload_to='DIRECION')
+
+    def __str__(self):
+        return f"{self.name} {self.category} {self.avaible_units} {self.price} {self.img}"
+    
+class neutro(models.neutro):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey()
+    avaible_units = models.FloatField(max_length=50)
+    price = models.FloatField(max_length=500)
+    img = models.ImageField(upload_to='DIRECION')
+
+    def __str__(self):
+        return f"{self.name} {self.category} {self.avaible_units} {self.price} {self.img}"
+    
+class dulces(models.dulces):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey()
+    avaible_units = models.FloatField(max_length=50)
+    price = models.FloatField(max_length=500)
+    img = models.ImageField(upload_to='DIRECION')
+
+    def __str__(self):
+        return f"{self.name} {self.category} {self.avaible_units} {self.price} {self.img}"

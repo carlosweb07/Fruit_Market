69ab2869bab7c8290a41b3b0c84bd601f9ff18de
JDRL555 <joshuleal3@gmail.com>
2024-04-09 10:16:28 -0400

    deleted the env file

--- fruit_market/settings.py
+++ fruit_market/settings.py
@@ -11,11 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
-
-import os
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,11 +78,11 @@ WSGI_APPLICATION = 'fruit_market.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST"),
-        "PORT": os.environ.get("DB_PORT"),
+        "NAME": "fruit_market",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": '127.0.0.1',
+        "PORT": 5432,
     }
 }
 

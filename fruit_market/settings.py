0ec5a6233632d658ecba8203c841db2dbd962dfd
gabriel moreno <gabriel2043579@gmail.com>
2024-04-13 16:37:29 -0400

    database connection password changed

--- fruit_market/settings.py
+++ fruit_market/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "fruit_market",
         "USER": "postgres",
-        "PASSWORD": "postgres",
+        "PASSWORD": "204402",
         "HOST": '127.0.0.1',
         "PORT": 5432,
     }
